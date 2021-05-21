d465b5efe8bc1f6018d9b2665ce48f690b5275c9
oshanQQ <onoyama1999910@gmail.com>
2021-05-21 14:03:31 +0900

    ✨ Set emoji

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import { Twemoji } from "react-emoji-render"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
