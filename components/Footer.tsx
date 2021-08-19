ae3a8d234d75f7e63478f9fc46bbf63e8a06795c
oshanQQ <oshanqq@gmail.com>
2021-08-20 01:08:02 +0900

    ✨ Make footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import { NextComponentType } from "next";
+
+const Footer: NextComponentType = () => {
+  return (
+    <>
+      <div className="flex justify-center pt-12 text-xl">
+        ©{new Date().getFullYear()},{` `}
+        <a href="https://twitter.com/oshanQQ">oshanqq</a>
+      </div>
+    </>
+  );
+};
+export default Footer;
