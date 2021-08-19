f4cc401179b555f276bae67e1539bbff6fbae51f
oshanQQ <oshanqq@gmail.com>
2021-08-20 01:07:44 +0900

    ➕ Add Japanese font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      ja: [
+        "Avenir",
+        "Helvetica Neue",
+        "Helvetica",
+        "Arial",
+        "Hiragino Sans",
+        "ヒラギノ角ゴシック",
+        "メイリオ",
+        "Meiryo",
+        "YuGothic",
+        "Yu Gothic",
+        "ＭＳ Ｐゴシック",
+        "MS PGothic",
+        "sans-serif",
+      ],
+    },
   },
   variants: {
     extend: {},
