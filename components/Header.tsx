4a9fcddf2796cd1b042b73cdf3897ee6d2fb40d9
oshanQQ <oshanqq@gmail.com>
2021-08-20 01:09:28 +0900

    ➕ Add link to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import type { NextComponentType } from "next";
+import Link from "next/link";
 
 const Header: NextComponentType = () => {
   return (
     <>
       <div>o-xian bolg</div>
+        <Link href="/">o-xian blog</Link>
     </>
   );
 };
