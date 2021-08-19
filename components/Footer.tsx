bb8b4c72424513991d3366011fae837179a4f71b
oshanQQ <oshanqq@gmail.com>
2021-08-20 01:29:28 +0900

    ♻️ Delete unused space

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: NextComponentType = () => {
   return (
     <>
       <div className="flex justify-center pt-12 text-xl">
-        ©{new Date().getFullYear()},{` `}
+        ©{new Date().getFullYear()},
         <a href="https://twitter.com/oshanQQ">oshanqq</a>
       </div>
     </>
