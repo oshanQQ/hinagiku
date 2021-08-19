17ea268a7d7bc11ea3da44d96d42a279dbdf2d8d
oshanQQ <oshanqq@gmail.com>
2021-08-19 23:56:18 +0900

    ✨ Make header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import type { NextComponentType } from "next";
+
+const Header: NextComponentType = () => {
+  return (
+    <>
+      <div>o-xian bolg</div>
+    </>
+  );
+};
+
+export default Header;
