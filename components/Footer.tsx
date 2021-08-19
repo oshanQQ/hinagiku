1703b789c09bd53b5934015682db2c62051af8e7
oshanQQ <oshanqq@gmail.com>
2021-08-20 01:28:27 +0900

    ♻️ Add new line

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,4 +10,5 @@ const Footer: NextComponentType = () => {
     </>
   );
 };
+
 export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,4 +10,5 @@ function MyApp({ Component, pageProps }: AppProps) {
     </Layout>
   );
 }
+
 export default MyApp;
