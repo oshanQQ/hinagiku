8e3ee4ba6ed4da8b35de8f1c58d8972a657b5cf7
oshanQQ <oshanqq@gmail.com>
2021-09-04 16:56:25 +0900

    📝 Fetch meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from "next";
+import fs from "fs";
+import matter from "gray-matter";
+import { PostMetaData } from "../interfaces/interfaces";
 
-const Home: NextPage = () => {
-  return <div className="text-2xl">寿限無寿限無後光の擦り切れ</div>;
+interface IProps {
+  posts: PostMetaData[];
+}
+
+const Home: NextPage<IProps> = ({ posts }) => {
+  return (
+    <div>
+      {posts.map((post, i) => (
+        <div key={i}>{post.title}</div>
+      ))}
+    </div>
+  );
 };
+
+export async function getStaticProps() {
+  const files = fs.readdirSync("posts");
+  let posts = files.map((file) => {
+    const data = fs.readFileSync(`posts/${file}`).toString();
+
+    return {
+      ...matter(data).data,
+      slug: file.split(".")[0],
+    };
+  });
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+}
 
 export default Home;
