4913b9edf488e700e86629555b2781c8c02bdcd3
oshanQQ <oshanqq@gmail.com>
2021-09-05 23:07:37 +0900

    📝 Add github notation

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import { FunctionComponent } from "react";
+import remarkGfm from "remark-gfm";
 
 import CodeBlock from "./CodeBlock";
 
@@ -14,7 +15,9 @@ const Markdown: FunctionComponent<IProps> = ({ content }) => {
 
   return (
     <div className="markdown">
-      <ReactMarkdown components={components}>{content}</ReactMarkdown>
+      <ReactMarkdown components={components} remarkPlugins={[remarkGfm]}>
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };
