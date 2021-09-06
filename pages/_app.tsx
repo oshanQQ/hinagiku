0ab85276029ef58a02347120225889a6f817ddaa
oshanQQ <oshanqq@gmail.com>
2021-09-06 16:17:37 +0900

    📝 Set meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 import "../styles/markdown.css";
 
 import Layout from "../components/Layout";
+import { myData } from "../data/myData";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>{myData.sitename}</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
