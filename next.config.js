72313cf43d1e10c3eee72cedd0c056f2f308b2f8
oshanQQ <oshanqq@gmail.com>
2021-09-06 16:57:28 +0900

    📝 Setting for pwa

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+  },
 });
---
+++ pages/_document.tsx
@@ -0,0 +1,63 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="ja-JP" dir="ltr">
+        <Head>
+          {/* windows */}
+          <meta
+            name="msapplication-square70x70logo"
+            content="/site-tile-70x70.png"
+          />
+          <meta
+            name="msapplication-square150x150logo"
+            content="/site-tile-150x150.png"
+          />
+          <meta
+            name="msapplication-wide310x150logo"
+            content="/site-tile-310x150.png"
+          />
+          <meta
+            name="msapplication-square310x310logo"
+            content="/site-tile-310x310.png"
+          />
+          <meta name="msapplication-TileColor" content="#fff" />
+          {/* safari */}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="#fff" />
+          <meta name="apple-mobile-web-app-title" content="hinagiku" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon-180x180.png"
+          />
+          {/* general */}
+          <meta name="application-name" content="hinagiku" />
+          <meta name="theme-color" content="#fff" />
+          <meta name="description" content="o-xian blog" />
+          <link rel="icon" sizes="192x192" href="/icon-192x192.png" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
