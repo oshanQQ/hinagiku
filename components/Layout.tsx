f289552307e1dd4c76a3ce5c6e640767def08c1d
oshanQQ <oshanqq@gmail.com>
2021-09-06 15:36:27 +0900

    ♻️ Refactor the codes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 const Layout: NextComponentType = ({ children }) => {
   return (
     <>
-      <div className="max-w-4xl px-8 py-8 mx-auto text-gray-600 font-ja">
+      <div className="max-w-4xl p-8 mx-auto text-gray-600 font-ja">
         <Header />
         <div>{children}</div>
         <Footer />
